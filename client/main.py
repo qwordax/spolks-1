611b957f2e9e05f6d68ae70598458640fa3479a8
Anatoly Nadolsky <a.nadolsky@proton.me>
2023-09-26 10:46:52 +0300

    `feat:` implement basic socket connection

--- client/main.py
+++ client/main.py
@@ -1,5 +1,16 @@
+import socket
+
+address = input('address: ')
+port = int(input('port: '))
+
+sock = socket.socket()
+
+sock.connect((address, port))
+
 while True:
     command = input('> ').strip() + '\n'
 
     if command == 'close\n':
         break
+
+sock.close()
--- server/main.py
+++ server/main.py
@@ -1,1 +1,16 @@
-print('Hello!')
+import socket
+
+address = input('address: ')
+port = int(input('port: '))
+
+sock = socket.socket()
+
+sock.bind((address, port))
+
+sock.listen(1)
+
+while True:
+    print('. . .')
+    conn, address = sock.accept()
+
+    conn.close()
