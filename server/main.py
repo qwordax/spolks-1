02e9f712a8f09fea12da257d35bb9b5df7ccd566
Anatoly Nadolsky <a.nadolsky@proton.me>
2023-09-27 10:41:23 +0300

    `fix:` change `WARNING` to `ERROR`

--- server/main.py
+++ server/main.py
@@ -9,7 +9,7 @@ port = int(input('port: '))
 
 logging.basicConfig(
     level=logging.INFO,
-    format='%(levelname)-7s : %(message)s'
+    format='%(levelname)-5s : %(message)s'
     )
 
 print('. . .')
@@ -43,8 +43,8 @@ while working:
             logging.info(command.strip())
             response = time.ctime() + '\n'
         else:
-            logging.warning(f'unknown command \'{command.strip()}\'')
-            response = f'unknown command \'{command.strip()}\'\n'
+            logging.error(f'unknown command \'{command.strip()}\'')
+            response = f'error: unknown command \'{command.strip()}\'\n'
 
         conn.send(response.encode('ascii'))
 
