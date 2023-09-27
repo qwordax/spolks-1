acea811bb2f3c69f3707bf1bed3658a2c766d12e
Anatoly Nadolsky <a.nadolsky@proton.me>
2023-09-27 08:52:45 +0300

    `feat!:` implement logging

--- server/main.py
+++ server/main.py
@@ -1,3 +1,4 @@
+import logging
 import socket
 import time
 
@@ -5,6 +6,11 @@ BUFSIZ = 1024
 
 address = input('address: ')
 port = int(input('port: '))
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(levelname)-8s%(message)s'
+    )
 
 print('. . .')
 
@@ -18,21 +24,29 @@ working = True
 
 while working:
     conn, address = sock.accept()
+    logging.info(f'accepted {address}')
 
     while True:
         command = conn.recv(BUFSIZ).decode('ascii')
+
+        if command == 'quit\n':
+            break
 
         if command == 'close\n':
             working = False
             break
 
         if command[:4] == 'echo':
-            response = command[5:].strip() + '\n'
+            logging.info(command.strip())
+            response = '\n'.join(command[5:].split()) + '\n'
         elif command[:4] == 'time':
+            logging.info(command.strip())
             response = time.ctime() + '\n'
         else:
+            logging.warning(f'unknown command \'{command.strip()}\'')
             response = '\n'
 
         conn.send(response.encode('ascii'))
 
+    logging.info(f'closed {address}')
     conn.close()
